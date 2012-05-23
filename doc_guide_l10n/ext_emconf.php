26f6584cf2d0ed04e63594ce05682016f960bac3
Christopher <chrissitopher@gmx.de>
2012-05-23 14:58:46 +0200

    [TASK] Migrate doc_guide_l10n 1.0.0
    Import doc_guide_l10n 1.0.0.
    
    Change-Id: Icaecc0b31d6f37a52ac43ec12969998f6d2b64c5

---
+++ doc_guide_l10n/ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "doc_l10nguide".
+#
+# Auto generated 03-10-2011 14:50
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Frontend Localization Guide',
+	'description' => '',
+	'category' => 'doc',
+	'shy' => 'false',
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '',
+	'PHP_version' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 'false',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 'false',
+	'lockType' => '',
+	'author' => 'Kasper SkÃ¥rhÃ¸j',
+	'author_email' => 'kasper@typo3.com',
+	'author_company' => 'Curby Soft Multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
